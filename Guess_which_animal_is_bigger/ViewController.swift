62e8315c80b48d76fda389df2a93a867dcefff47
slavabily <slavabily@me.com>
2020-01-23 18:11:54 +0200

    Initial Commit

---
+++ Guess_which_animal_is_bigger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess_which_animal_is_bigger
+//
+//  Created by slava bily on 23/1/20.
+//  Copyright © 2020 slava bily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
