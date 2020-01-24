2aa237e7f90e5c792dfc6e74292cc8c0b48eacb7
slavabily <slavabily@me.com>
2020-01-24 15:38:00 +0200

    initial view creation

--- Guess_which_animal_is_bigger/ViewController.swift
+++ Guess_which_animal_is_bigger/ViewController.swift
@@ -9,11 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let button1 = UIButton()
+    let button2 = UIButton()
+    let button3 = UIButton()
+    
+    let scoreLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30)
+        ])
+ 
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        scoreLabel.textAlignment = .center
+        scoreLabel.attributedText = NSAttributedString(string: "Score: 0", attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .largeTitle)])
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        button1.translatesAutoresizingMaskIntoConstraints = false
+        button2.translatesAutoresizingMaskIntoConstraints = false
+        button3.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.addArrangedSubview(button1)
+        stackView.addArrangedSubview(button2)
+        stackView.addArrangedSubview(button3)
+        stackView.addArrangedSubview(scoreLabel)
+        
+        stackView.setCustomSpacing(30, after: button1)
+        stackView.setCustomSpacing(30, after: button2)
+        stackView.distribution = .fillEqually
+
+        
+     }
 
 
 }
