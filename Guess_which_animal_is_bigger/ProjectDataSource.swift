428e9c069313ee3d6f4abbad25728de5afa44b6a
slavabily <slavabily@me.com>
2020-01-26 22:06:12 +0200

    refacored between data source and view controller

---
+++ Guess_which_animal_is_bigger/ProjectDataSource.swift
@@ -0,0 +1,73 @@
+//
+//  ProjectDataSource.swift
+//  Guess_which_animal_is_bigger
+//
+//  Created by slava bily on 26/1/20.
+//  Copyright © 2020 slava bily. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ProjectDataSource: NSObject {
+    
+    var animals = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
+    var button1Name: String!
+    var button2Name: String!
+    var button3Name: String!
+    
+    func prepareQuestion() {
+        
+        animals.append("elephant")
+        animals.append("dolphin")
+        animals.append("bear")
+        animals.append("crocodile")
+        animals.append("deer")
+        animals.append("wolf")
+        
+        animals = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: animals) as! [String]
+        
+        button1Name = animals[0]
+        button2Name = animals[1]
+        button3Name = animals[2]
+    }
+    
+    func prepareAnswer() {
+        let buttonsNames = [button1Name, button2Name, button3Name]
+        var animalsSizes = [Int]()
+        
+        for buttonName in buttonsNames {
+            switch buttonName {
+            case "elephant":
+                animalsSizes.append(6)
+            case "crocodile":
+                animalsSizes.append(5)
+            case "deer":
+                animalsSizes.append(3)
+            case "wolf":
+                animalsSizes.append(1)
+            case "dolphin":
+                animalsSizes.append(4)
+            case "bear":
+                animalsSizes.append(2)
+            default:
+                break
+            }
+        }
+        
+        if let maxSizeAnimal = animalsSizes.max() {
+            if let index = animalsSizes.firstIndex(of: maxSizeAnimal) {
+                correctAnswer = index
+            } else {
+                fatalError("Size of max animal could no be found.")
+            }
+            print("Max size animal number:\(maxSizeAnimal) \n", "correctAnswer:\(correctAnswer)")
+            
+        } else {
+            fatalError("Max size animal could not be found.")
+        }
+    }
+}
--- Guess_which_animal_is_bigger/ViewController.swift
+++ Guess_which_animal_is_bigger/ViewController.swift
@@ -13,13 +13,7 @@ class ViewController: UIViewController, Storyboarded {
     
     weak var coordinator: MainCoordinator?
     
-    var animals = [String]()
-    var correctAnswer = 0
-    var score = 0
-    
-    var button1Name: String!
-    var button2Name: String!
-    var button3Name: String!
+    let dataSource = ProjectDataSource()
     
     let button1 = UIButton()
     let button2 = UIButton()
@@ -29,13 +23,6 @@ class ViewController: UIViewController, Storyboarded {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        animals.append("elephant")
-        animals.append("dolphin")
-        animals.append("bear")
-        animals.append("crocodile")
-        animals.append("deer")
-        animals.append("wolf")
         
         navigationItem.largeTitleDisplayMode = .never
         
@@ -50,6 +37,10 @@ class ViewController: UIViewController, Storyboarded {
             stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
             stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30)
         ])
+        
+        button1.tag = 0
+        button2.tag = 1
+        button3.tag = 2
  
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
@@ -79,20 +70,12 @@ class ViewController: UIViewController, Storyboarded {
      }
     
     func askQuestion(action: UIAlertAction! = nil) {
+        dataSource.prepareQuestion()
         
-        animals = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: animals) as! [String]
+        button1.setImage(UIImage(named: dataSource.button1Name), for: .normal)
+        button2.setImage(UIImage(named: dataSource.button2Name), for: .normal)
+        button3.setImage(UIImage(named: dataSource.button3Name), for: .normal)
         
-        button1Name = animals[0]
-        button2Name = animals[1]
-        button3Name = animals[2]
-        
-        button1.setImage(UIImage(named: button1Name), for: .normal)
-        button2.setImage(UIImage(named: button2Name), for: .normal)
-        button3.setImage(UIImage(named: button3Name), for: .normal)
-        
-        button1.tag = 0
-        button2.tag = 1
-        button3.tag = 2
         
         button1.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
         button2.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
@@ -100,63 +83,26 @@ class ViewController: UIViewController, Storyboarded {
         
         title = "Which animal is bigger?"
         
-        compare()
-    }
-    
-    func compare() {
-        let buttonsNames = [button1Name, button2Name, button3Name]
-        var animalsSizes = [Int]()
-        
-        for buttonName in buttonsNames {
-            switch buttonName {
-            case "elephant":
-                animalsSizes.append(6)
-            case "crocodile":
-                animalsSizes.append(5)
-            case "deer":
-                animalsSizes.append(3)
-            case "wolf":
-                animalsSizes.append(1)
-            case "dolphin":
-                animalsSizes.append(4)
-            case "bear":
-                animalsSizes.append(2)
-            default:
-                break
-            }
-        }
-        
-        if let maxSizeAnimal = animalsSizes.max() {
-            if let index = animalsSizes.firstIndex(of: maxSizeAnimal) {
-                correctAnswer = index
-            } else {
-                fatalError("Size of max animal could no be found.")
-            }
-            print("Max size animal number:\(maxSizeAnimal) \n", "correctAnswer:\(correctAnswer)")
-            
-        } else {
-            fatalError("Max size animal could not be found.")
-        }
+        dataSource.prepareAnswer()
     }
     
    @objc func buttonTapped(_ sender: UIButton) {
         
         var title: String
         
-        if sender.tag == correctAnswer {
+        if sender.tag == dataSource.correctAnswer {
             title = "Correct"
-            score += 1
+            dataSource.score += 1
         } else {
             title = "Wrong"
-            score -= 1
+            dataSource.score -= 1
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        let ac = UIAlertController(title: title, message: "Your score is \(dataSource.score)", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion(action:)))
         present(ac, animated: true)
         
-         scoreLabel.attributedText = NSAttributedString(string: "Score: \(score)", attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .largeTitle)])
+         scoreLabel.attributedText = NSAttributedString(string: "Score: \(dataSource.score)", attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .largeTitle)])
     }
-    
 }
 
